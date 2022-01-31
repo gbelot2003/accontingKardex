a4a81eb32e8a1d4fd44f576dc1044c17f13ca716
Gerardo A Belot <gbelot2003@hotmail.com>
2022-01-31 02:37:21 +0000

    💄 UI: comprobando links

--- resources/js/components/Sidebar.vue
+++ resources/js/components/Sidebar.vue
@@ -8,12 +8,6 @@
             Dashboard
           </router-link>
         </li>
-        <li>
-          <router-link to="/about">About</router-link>
-        </li>
-        <li>
-          <router-link to="/contact">Contact Us</router-link>
-        </li>
       </ul>
     </div>
   </nav>
